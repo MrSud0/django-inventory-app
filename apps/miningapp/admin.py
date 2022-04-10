2784b3e6bc8df954794cc23666e463fac72bac60
Anihilakos <jason_somarakis@protonmail.com>
2022-04-10 21:17:56 +0300

    test

--- apps/miningapp/admin.py
+++ apps/miningapp/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from apps.miningapp.models import Hashrate,Motherboard,Coin, GPU
 from django.views import generic
 from apps.miningapp.models import *
 from django import template
@@ -7,12 +6,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
+from django.apps import apps
 # Register your models here.
-admin.site.register(Hashrate)
-admin.site.register(Motherboard)
-admin.site.register(Coin)
-
+from django.contrib.admin.sites import AlreadyRegistered
 
 class GPUAdmin(admin.ModelAdmin):
     list_display = ('name', 'model', 'price', 'wattage')
@@ -21,4 +17,4 @@ class GPUAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(GPU, GPUAdmin)
+admin.site.register(GPUAdmin)
