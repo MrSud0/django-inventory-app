358d41bc1123e669d543d4c17f6f4d71d75f9f1d
Anihilakos <jason_somarakis@protonmail.com>
2022-04-10 11:54:15 +0300

    static files paths

--- core/settings.py
+++ core/settings.py
@@ -123,10 +123,6 @@ USE_TZ = True
 #     os.path.join(CORE_DIR, 'apps/static'),
 # )
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
@@ -135,8 +131,15 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# Activate Django-Heroku.
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
 #############################################################
 #############################################################
