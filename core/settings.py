a5f50076d411a87f561cd3d8a4923a1ba96d0d9c
Anihilakos <jason_somarakis@protonmail.com>
2022-04-10 13:16:51 +0300

    settings.py update

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 import os
 from decouple import config
 from unipath import Path
-import psycopg2
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
