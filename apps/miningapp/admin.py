19bd11e1dfaf5f221e0d49f8b97e762fa18a2701
Anihilakos <jason_somarakis@protonmail.com>
2022-04-10 14:59:50 +0300

    admin site changes

--- apps/miningapp/admin.py
+++ apps/miningapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from apps.miningapp.models import Hashrate,Motherboard,Coin, GPU
 # Register your models here.
 admin.site.register(Hashrate)
 admin.site.register(Motherboard)
